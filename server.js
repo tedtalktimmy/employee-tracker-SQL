06ea792f9b4371f05bdf0baa700d5f5eb3402b57
tedtalktimmy <frtntn007@gmail.com>
2022-10-24 09:03:51 -0400

    fixed error i was having

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection(
 
 const init = () => {
 
-let response = inquirer.prompt(
+inquirer.prompt([
   {
       message: 'what would you like to do?',
       type: 'list',
@@ -28,36 +28,42 @@ let response = inquirer.prompt(
         'add an employee',
         'update an employee role'
       ],
-    }).then((response => {
+    }]).then((response) => {
       switch (response.choice) {
         case 'view all departments':
           viewAllDepartments();
+          break;
 
 
         case 'view all roles':
           viewAllRoles();
+          break;
 
         case 'view all employees':
-          return viewAllEmployees();
+          viewAllEmployees();
+          break;
 
-        case 'add department':
+        case 'add a department':
           addDepartment();
+          break;
 
-        case 'add role':
+        case 'add a role':
           addRole();
+          break;
 
-        case 'add employee':
+        case 'add an employee':
           addEmployee();
+          break;
 
         case 'update employee role':
           updateEmployeeRole();
+          break;
 
         case 'exit':
           console.log('Until next time!');
           process.exit(0);
       }
     })
-    )
 };
 
 
@@ -83,25 +89,25 @@ const viewAllEmployees = () => {
 };
 
 const addDepartment = () => {
-  db.promise().query(`SELECT name, id FROM departments`).then(([deptResults, fields]) => {
-    const departments = deptResults.map(departments => {
-      return { name: departments.name, value: departments.id }
-    })
+  console.log("inside add department");
+  // db.promise().query(`SELECT name, id FROM departments`).then(([deptResults, fields]) => {
+  //   const departments = deptResults.map(departments => {
+  //     return { name: departments, value: departments.id }
+  //   })
     inquirer.prompt([
       {
         message: 'enter the name of the department',
         type: 'input',
-        name: 'departmentName'
+        name: 'name'
       },
     ])
       .then((response) => {
-        db.promise().query(`INSERT INTO departments (name)VALUES(?)`, [response.departmentName]).then(([results, fields]) => {
+        db.promise().query(`INSERT INTO departments (name)VALUES(?)`, [response.name]).then(([results, fields]) => {
           console.table(results);
           init();
         });
       });
-  });
-};
+  };
 
 const addRole = () => {
   db.promise().query(`SELECT title, salary, id FROM roles`).then(([roleResults, fields]) => {
@@ -112,7 +118,7 @@ const addRole = () => {
       const departments = deptResults.map(departments => {
         return { name: departments.name, value: departments.id }
       })
-      inquirer.prompt([
+      let response = inquirer.prompt([
         {
           message: 'enter role',
           type: 'input',
@@ -141,10 +147,10 @@ const addRole = () => {
 
 const addEmployee = () => {
   db.promise().query(`SELECT first_name, last_name, id FROM employees`).then(([results, fields]) => {
-    const managers = results.map(employee => { return { name: employee.first_name + ' ' + employee.last_name, value: employee.id } })
+    const managers = results.map(employees => { return { name: employees.first_name + ' ' + employees.last_name, value: employees.id } })
     db.promise().query(`SELECT title, id FROM roles`).then(([roleResults, fields]) => {
       const roles = roleResults.map(roles => { return { name: roles.title, value: roles.id } })
-      inquirer.prompt([
+       let response = inquirer.prompt([
         {
           message: 'please enter the first name of the employee',
           type: 'input',
@@ -180,30 +186,30 @@ const addEmployee = () => {
 
 const updateEmployeeRole = () => {
   db.promise().query(`SELECT first_name, last_name, id FROM employees`).then(([results, fields]) => {
-    const employees = results.map(employee => {
+    const employees = results.map(employees => {
       return {
-        name: employee.first_name + ' ' + employee.last_name, value: employee.id
+        name: employees.first_name + ' ' + employees.last_name, value: employees.id
       }
     })
-    db.promise().query(`SELECT title, id FROM roles`).then(([roleResults, fields]) => {
-      const roles = roleResults.map(role => {
-        return { name: role.title, value: role.id }
+    db.promise().query(`SELECT title, id FROM roles`).then(([rolesResults, fields]) => {
+      const roles = rolesResults.map(role => {
+        return { name: roles.title, value: roles.id }
       });
       inquirer.prompt([
         {
           message: 'which employee?',
           choices: employees,
           type: 'list',
-          name: 'employee_name,'
+          name: 'employeesName,'
         },
         {
           message: 'please select new role for employee',
           choices: roles,
           type: 'list',
-          name: 'role'
+          name: 'roles'
         },
       ]).then((response) => {
-        db.promise().query(`UPDATE employees SET role_id = ? WHERE employee.id = ?`, [response.role, response.employee_name]).then(([results, fields]) => {
+        db.promise().query(`UPDATE employees SET role_id = ? WHERE employees.id = ?`, [response.roles, response.employeesName]).then(([results, fields]) => {
           console.table(results);
           init();
         });
